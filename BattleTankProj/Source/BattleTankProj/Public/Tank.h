574e13b019c8543ab22a4bfd0badedd20e5406b6
awicks44 <awicks44@gmail.com>
2018-09-13 17:18:55 -0400

    Creating Default sub objects - https://www.udemy.com/unrealcourse/learn/v4/t/lecture/5242838?start=0 - Adding an Aiming Component to the tank blueprint using C++ and delegate behavior to this component - Aiming component will now handle logging of hit location to screen instead of Tank class. Tank class has a reference to aiming component

--- BattleTankProj/Source/BattleTankProj/Public/Tank.h
+++ BattleTankProj/Source/BattleTankProj/Public/Tank.h
@@ -2,9 +2,10 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
-#include "Tank.generated.h"
+#include "Tank.generated.h" // paste new classes above this
 
 UCLASS()
 class BATTLETANKPROJ_API ATank : public APawn
@@ -12,16 +13,17 @@ class BATTLETANKPROJ_API ATank : public APawn
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
-	ATank();
-
 	void AimAt(FVector HitLocation);
 
-protected:
+protected: // can't be proviate because we will need to access it using a UProperty later
+	UTankAimingComponent * TankAimingComponent = nullptr;
+	
+
+private:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-private:	
+	// Sets default values for this pawn's properties
+	ATank();
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
